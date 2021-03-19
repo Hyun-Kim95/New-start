9cad793b797dc03ba6cbc82cd78641c9d98705cd
Hyun-Kim95 <75933619+Hyun-Kim95@users.noreply.github.com>
2021-03-20 00:08:51 +0900

    Add files via upload

--- lotto.py
+++ lotto.py
@@ -3,10 +3,12 @@ from openpyxl import load_workbook
 wb = load_workbook("lotto.xlsx")
 ws = wb.active
 
-num = [] * 6
 alpha = [ "J", "K", "L", "M", "N", "O"]
 maxalpha = ["Q1", "R1", "S1", "T1", "U1", "V1"]
+average1 = ["Q2", "R2", "S2", "T2", "U2", "V2"]
+average2 = ["Q3", "R3", "S3", "T3", "U3", "V3"]
 maxalpha2 = ["Q4", "R4", "S4", "T4", "U4", "V4"]
+s = ["W1", "W2", "W3"]
 
 while True:
     for i in range(6):
@@ -39,9 +41,19 @@ while True:
 
         counts = collections.Counter(다음숫자)
 
-        ws[maxalpha[i]].value = counts.most_common(2)[0][0]
+        ws[maxalpha[i]].value = counts.most_common(1)[0][0]
+        ws[average1[i]].value = "=AVERAGE({}1:{}{})".format(alpha[i], alpha[i],len(다음숫자)-1)
+        ws[average2[i]].value = "=AVERAGE({},{})".format(maxalpha[i], average1[i])
 
     break
+for i in range(1,4):
+    ws[s[i-1]] = "=SUM(Q{}:V{})".format(i,i)
+j = 1
+while True:
+    ws["P{}".format(j)] = "=SUM(J{}:O{})".format(j, j)
+    j += 1
+    if ws["J{}".format(j)].value == None:
+        break
 
 wb.save("lotto1.xlsx")
 wb.close()
